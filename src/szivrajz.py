a459f89222bbd22be4a3f5ddb79653751500d966
AronSzabo2003 <szaboaron2003@gmail.com>
2025-04-01 10:49:53 +0200

    szivet rajzol

--- src/szivrajz.py
+++ src/szivrajz.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
@@ -8,22 +10,22 @@ class HeartDraw(Node):
         super().__init__('szivrajz')
         self.publisher_ = self.create_publisher(Twist, '/turtlesim1/turtle1/cmd_vel', 10)
 
-        
         self.steps = [
-            (2.0, math.pi / 4, 1.5),   
-            (0.0, -math.pi / 2, 1.0),  
-            (2.0, -math.pi / 4, 1.5),  
-            (2.0, math.pi / 4, 1.5),   
-            (0.0, -math.pi / 2, 1.0), 
-            (2.0, -math.pi / 4, 1.5), 
-            (2.0, 0.0, 2.0),          
-            (0.0, 0.0, 1.0)           
+            (0.0, math.radians(120), 1.0),  # 90°-os balra fordulás (kezdés)
+            (2.0, 0.0, 1.0),          # Egyenes felfelé
+            (0.0, -math.radians(300), 0.5), # Jobbra fordulás 140°
+            (1.0, 0.0, 1.0),          # Jobbra egyenes
+            (0.0,math.radians(150), 0.5), # Jobbra fordulás 90°
+            (1.0, 0.0, 1.0),          # Lefelé egyenes
+            (0.0, -math.radians(320), 0.5), # Jobbra fordulás 90°
+            (2.0, 0.0, 1.0),          # Lefelé befejezés
+            (0.0, 0.0, 1.0)           # Leállás
         ]
-        
-        self.current_step = 0
-        self.timer = self.create_timer(0.1, self.loop)  
 
-        self.get_logger().info("Szív rajzolás indul a Turtlesim-ben.")
+        self.current_step = 0
+        self.timer = self.create_timer(0.1, self.loop)
+
+        self.get_logger().info("Szögletes szív rajzolása indul.")
 
     def publish_message(self, fwd, turn):
         """Sebesség és fordulás elküldése a teknősnek"""
@@ -39,10 +41,10 @@ class HeartDraw(Node):
             fwd, turn, duration = self.steps[self.current_step]
             self.publish_message(fwd, turn)
             self.current_step += 1
-            self.timer.cancel() 
+            self.timer.cancel()
             self.timer = self.create_timer(duration, self.loop)  
         else:
-            self.get_logger().info("Szív rajzolás kész. Leállítás.")
+            self.get_logger().info("Szögletes szív kész. Leállítás.")
             self.publish_message(0.0, 0.0)  
             self.timer.cancel()
             rclpy.shutdown()
